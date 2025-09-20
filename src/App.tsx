c975ba32883ad20611c7f3c264685108dd24ad3e
pavants777 <pavangowdats10@gmail.com>
2025-09-20 21:52:12 +0530

    404 add

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -8,25 +8,34 @@ import SchoolDetailsPage from './pages/Facilities';
 import Academics from './pages/Academics';
 import Contact from './pages/Contact';
 import AlumniYear from './pages/AlumniYear';
+import NotFound from './pages/NotFound';
+
+// Layout wrapper with header/footer
+const Layout: React.FC = () => (
+  <div className="min-h-screen flex flex-col">
+    <Header />
+    <main className="flex-1">
+      <Outlet /> 
+    </main>
+    <Footer />
+  </div>
+);
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/facilities" element={<SchoolDetailsPage />} />
-            <Route path="/academics" element={<Academics />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/alumni" element={<AlumniYear />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/facilities" element={<SchoolDetailsPage />} />
+          <Route path="/academics" element={<Academics />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/alumni" element={<AlumniYear />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
 }
