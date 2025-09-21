e53d82ae00be81396601a82fcacd42243a944d21
pavants777 <pavangowdats10@gmail.com>
2025-09-21 12:49:32 +0530

    struct

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -11,31 +11,75 @@ import AlumniYear from './pages/AlumniYear';
 import NotFound from './pages/NotFound';
 import ScrollToTop from './components/ScrollToTop';
 
-const Layout: React.FC = () => (
-  <div className="min-h-screen flex flex-col">
-    <Header />
-    <main className="flex-1">
-      <ScrollToTop />
-      <Outlet />
-    </main>
-    <Footer />
-  </div>
-);
-
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/facilities" element={<SchoolDetailsPage />} />
-          <Route path="/academics" element={<Academics />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/alumni" element={<AlumniYear />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
+        <Route
+          path="/" element={
+            <>
+              <Header />
+              <Home />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/home"
+          element={
+            <>
+              <Header />
+              <Home />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/about"
+          element={
+            <>
+              <Header />
+              <About />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/facilities"
+          element={
+            <>
+              <Header />
+              <SchoolDetailsPage />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/academics"
+          element={
+            <>
+              <Header />
+              <Academics />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/contact"
+          element={
+            <>
+              <Header />
+              <Contact />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/alumni"
+          element={
+            <>
+              <Header />
+              <AlumniYear />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
